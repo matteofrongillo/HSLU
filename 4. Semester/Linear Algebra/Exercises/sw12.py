918c5aba595ed594ac9bae0648ef163453223ef0
Matteo Frongillo <167030488+matteofrongillo@users.noreply.github.com>
2026-05-08 10:20:08 +0200

    11 SW

---
+++ 4. Semester/Linear Algebra/Exercises/sw12.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+A = np.array([[1,2],[3,4]])
+B = np.array([[1,2],[3,4],[5,6]])
+C = np.array([[1,2,3],[4,5,6]])
+
+# print(B.shape, B.size)
+
+D = np.hstack([A,C])
+print(D)
+
